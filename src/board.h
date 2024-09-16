67730fc0fcf0c00bfe3ae5e2de6f3da683c1c460
Ampherost <Ampherost235@gmail.com>
2024-09-16 16:26:07 -0700

    Added board definition

--- src/board.h
+++ src/board.h
@@ -1,4 +1,30 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#endif
+#include <vector>
+#include <iostream>
+
+class Board {
+public:
+    Board();
+
+    void displayBoard() const;
+
+    bool placePiece(int column, char piece);
+
+    bool checkWin(char piece) const;
+
+    bool isFull() const;
+
+private:
+    std::vector<std::vector<char>> grid;
+
+    bool checkHorizontalWin(char piece) const;
+    bool checkVerticalWin(char piece) const;
+    bool checkDiagonalWin(char piece) const;
+
+    static const int ROWS = 6;
+    static const int COLUMNS = 7;
+};
+
+#endif 
